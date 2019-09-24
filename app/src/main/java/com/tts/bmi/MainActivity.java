10014a95ee89d5fd92274279cdc2cfff7bf9b89b
TTS <thomas871208@gmail.com>
2019-09-24 16:36:01 +0800

    first commit

---
+++ app/src/main/java/com/tts/bmi/MainActivity.java
@@ -0,0 +1,24 @@
+package com.tts.bmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.box);
+    }
+}
+
+
+
+
+
+
+
+
+
+
